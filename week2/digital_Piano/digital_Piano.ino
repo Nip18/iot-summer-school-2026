e8d252adf919127c5f5247409e1cc8f621b2d941
Nipun S Pandkar <66487303+Nip18@users.noreply.github.com>
2026-07-05 13:59:38 +0530

    new feature2
    integrate 5th button to toggle Major and Minor scales

--- week2/digital_Piano/digital_Piano.ino
+++ week2/digital_Piano/digital_Piano.ino
@@ -3,9 +3,14 @@ const int BUTTON_DO = 2;
 const int BUTTON_RE = 3;
 const int BUTTON_MI = 4;
 const int BUTTON_FA = 5;
+const int TOGGLE_MODE_PIN = 6;
 
-// basic frequencies for major scale notes(fetched)
-const int scaleNotes[] = {262, 294, 330, 349}; 
+// frequencies: Do, Re, Mi, Fa, Sol
+const int majorScale[] = {262, 294, 330, 349, 392}; 
+const int minorScale[] = {262, 294, 311, 349, 392}; // Mi-flat at 311Hz for minor
+
+bool isMajorMode = true;
+bool lastToggleState = HIGH;
 
 void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
@@ -13,27 +18,41 @@ void setup() {
   pinMode(BUTTON_RE, INPUT_PULLUP);
   pinMode(BUTTON_MI, INPUT_PULLUP);
   pinMode(BUTTON_FA, INPUT_PULLUP);
+  pinMode(TOGGLE_MODE_PIN, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 
 void loop() {
+  // read and debounce scale toggle button state
+  bool currentToggleState = digitalRead(TOGGLE_MODE_PIN);
+  if (currentToggleState == LOW && lastToggleState == HIGH) {
+    delay(50); // software debouncing window
+    if (digitalRead(TOGGLE_MODE_PIN) == LOW) {
+      isMajorMode = !isMajorMode;
+      Serial.print("Active Scale: ");
+      Serial.println(isMajorMode ? "MAJOR" : "MINOR");
+    }
+  }
+  lastToggleState = currentToggleState;
+
   bool pressDo = (digitalRead(BUTTON_DO) == LOW);
   bool pressRe = (digitalRead(BUTTON_RE) == LOW);
   bool pressMi = (digitalRead(BUTTON_MI) == LOW);
   bool pressFa = (digitalRead(BUTTON_FA) == LOW);
 
-  // check if two or more buttons are pressed simultaneously
   int pressedCount = pressDo + pressRe + pressMi + pressFa;
+  const int* activeScale = isMajorMode ? majorScale : minorScale;
 
   if (pressedCount >= 2) {
-    tone(BUZZER_PIN, 392); // play Sol (392Hz) as a chord substitute
+    tone(BUZZER_PIN, activeScale[4]); // Sol chord note
   } else if (pressDo) {
-    tone(BUZZER_PIN, 262);
+    tone(BUZZER_PIN, activeScale[0]);
   } else if (pressRe) {
-    tone(BUZZER_PIN, 294);
+    tone(BUZZER_PIN, activeScale[1]);
   } else if (pressMi) {
-    tone(BUZZER_PIN, 330);
+    tone(BUZZER_PIN, activeScale[2]);
   } else if (pressFa) {
-    tone(BUZZER_PIN, 349);
+    tone(BUZZER_PIN, activeScale[3]);
   } else {
     noTone(BUZZER_PIN);
   }
